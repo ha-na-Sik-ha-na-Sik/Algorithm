31e807768d88f7da4ba9611f8c01ca01774d62fe
백채연 <codusl100@naver.com>
2023-02-18 13:32:26 +0900

    성공 : section 01 - 03 문장 속 단어

---
+++ src/main/java/org/example/seolki/section01/Problem03.java
@@ -0,0 +1,17 @@
+package org.example.seolki.section01;
+import java.util.*;
+
+public class problem03{
+public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        String max = "";
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].length() >= max.length()){
+               max = arr[i];
+                }
+            }
+            System.out.println(max);
+            }
+        }
