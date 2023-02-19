6dbb3b636fdd055b58afa1b3673fdcec3ba8b9a5
HunSeongPark <phunseong@gmail.com>
2023-02-19 14:24:55 +0900

    성공: section 02-02 보이는 학생

---
+++ src/main/java/org/example/bbakoon/section2/Problem02.java
@@ -0,0 +1,27 @@
+package org.example.bbakoon.section2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
