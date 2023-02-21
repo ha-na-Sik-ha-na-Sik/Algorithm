0dfa81d55698494ff41db17cbe43c078423ec899
jackson <steady8@kakao.com>
2023-02-21 10:56:02 +0900

    성공: section02-07 점수 계산

--- src/main/java/org/example/jackson/section02/Problem07.java
+++ src/main/java/org/example/jackson/section02/Problem07.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Problem07 {
 
-    public static int solution(int N, int[] arr) {
-        int answer = 0, cnt = 0;
+    public static int[] solution(int N, int[] arr) {
+        int[] answer = new int[N];
 
         for (int i = 0; i < N; i++) {
-            if (arr[i] == 1) {
-                answer += ++cnt;
-            } else {
-                cnt = 0;
+            int rank = 1;
+            for (int k = 0; k < N; k++) {
+                if (arr[k] > arr[i]) rank++;
             }
+            answer[i] = rank;
         }
+
         return answer;
     }
 
@@ -26,7 +27,9 @@ public class Problem07 {
             arr[i] = scan.nextInt();
         }
 
-        System.out.println(solution(N, arr));
+        for (int i : solution(N, arr)) {
+            System.out.print(i + " ");
+        }
 
     }
 }
