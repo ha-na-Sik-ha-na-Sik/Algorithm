8eb208e73e18ea4a397ecdf2e90da70fdacc7b3b
jackson <steady8@kakao.com>
2023-02-21 10:50:46 +0900

    성공: section02-08 등수 구하기

--- src/main/java/org/example/jackson/section02/Problem07.java
+++ src/main/java/org/example/jackson/section02/Problem07.java
@@ -4,29 +4,34 @@ import java.util.Scanner;
 
 public class Problem07 {
 
-    public static int solution(int N, int[] arr) {
-        int answer = 0, cnt = 0;
+    public static int[] solution(int N, int[] arr) {
+        int[] answer = new int[N];
 
         for (int i = 0; i < N; i++) {
-            if (arr[i] == 1) {
-                answer += ++cnt;
-            } else {
-                cnt = 0;
+            int rank = 1;
+            for (int k = 0; k < N; k++) {
+                if (arr[k] > arr[i]) rank++;
             }
+            answer[i] = rank;
         }
+
         return answer;
     }
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         int N = scan.nextInt();
         int[] arr = new int[N];
+
         for (int i = 0; i < N; i++) {
             arr[i] = scan.nextInt();
         }
 
-        System.out.println(solution(N, arr));
+        for (int i : solution(N, arr)) {
+            System.out.print(i + " ");
+        }
 
     }
 }
